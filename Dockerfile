26a135b7c4a678c564f227bd922c697f2acd404d
Lyubomir Hadzhiev <lubomir.hadziev@gmail.com>
2020-08-04 16:21:57 +0300

    init

---
+++ Dockerfile
@@ -0,0 +1,28 @@
+FROM python:3.8-alpine as base
+FROM base as builder
+
+COPY requirements.txt /app/
+COPY setup.py /app/
+COPY README.md /app/
+COPY proxy/ /app/proxy/
+WORKDIR /app
+RUN pip install --upgrade pip && \
+    pip install --prefix="/deps" .
+
+FROM base
+
+LABEL com.abhinavsingh.name="abhinavsingh/proxy.py" \
+      com.abhinavsingh.description="⚡⚡⚡ Fast, Lightweight, Pluggable, TLS interception capable proxy server focused on \
+        Network monitoring, controls & Application development, testing, debugging." \
+      com.abhinavsingh.url="https://github.com/abhinavsingh/proxy.py" \
+      com.abhinavsingh.vcs-url="https://github.com/abhinavsingh/proxy.py" \
+      com.abhinavsingh.docker.cmd="docker run -it --rm -p 8899:8899 abhinavsingh/proxy.py"
+
+COPY --from=builder /deps /usr/local
+
+# Install openssl to enable TLS interception within container
+RUN apk update && apk add openssl
+
+EXPOSE 8899/tcp
+ENTRYPOINT [ "proxy" ]
+CMD [ "--hostname=0.0.0.0" ]
---
+++ proxy/plugin/man_in_the_middle.py
@@ -0,0 +1,56 @@
+# -*- coding: utf-8 -*-
+"""
+    proxy.py
+    ~~~~~~~~
+    ⚡⚡⚡ Fast, Lightweight, Pluggable, TLS interception capable proxy server focused on
+    Network monitoring, controls & Application development, testing, debugging.
+
+    :copyright: (c) 2013-present by Abhinav Singh and contributors.
+    :license: BSD, see LICENSE for more details.
+"""
+import logging
+import socket
+from typing import Optional
+
+from ..common.utils import build_http_response
+from ..http.parser import HttpParser
+from ..http.codes import httpStatusCodes
+from ..http.proxy import HttpProxyBasePlugin
+
+logger = logging.getLogger(__name__)
+
+
+class ManInTheMiddlePlugin(HttpProxyBasePlugin):
+    """Modifies upstream server responses."""
+
+    def before_upstream_connection(
+        self, request: HttpParser) -> Optional[HttpParser]:
+        address = socket.gethostbyname(socket.gethostname())
+        request.add_header('X-Forwarded-For', bytes('%s' % address, encoding='UTF-8'))
+
+        return request
+
+    def handle_client_request(
+        self, request: HttpParser) -> Optional[HttpParser]:
+        address = socket.gethostbyname(socket.gethostname())
+        request.add_header('X-Forwarded-For', bytes('%s' % address, encoding='UTF-8'))
+
+        return request
+
+    def handle_upstream_chunk(self, chunk: memoryview) -> memoryview:
+        address = socket.gethostbyname(socket.gethostname())
+
+        logger.error('Access from IP %s' % address)
+
+        return chunk
+
+        # return memoryview(build_http_response(
+        #     httpStatusCodes.OK,
+        #     reason=b'OK', body=bytes('%s' % address, encoding='UTF-8')))
+
+        return memoryview(build_http_response(
+            httpStatusCodes.OK,
+            reason=b'OK', body=b'Hello from man in the middle'))
+
+    def on_upstream_connection_close(self) -> None:
+        pass
